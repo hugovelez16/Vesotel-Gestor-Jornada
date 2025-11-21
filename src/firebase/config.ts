65b5eeb51d33617835a6ca2c3e7c68d1ef634ff5
Hugo Vélez <hugo@vesotel.com>
2025-11-21 20:13:16 +0000

    lo que tenemos q hacer es vincular con el proyecto vesotel-gestor-jornad

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-5177963256-f3dc5",
+  "projectId": "vesotel-gestor-jornada",
   "appId": "1:1088612949766:web:f1bb7c2bf29563dc434887",
   "apiKey": "AIzaSyAkkt59I1DKzT_EBm33NFMwEOJGnJkC5kA",
-  "authDomain": "studio-5177963256-f3dc5.firebaseapp.com",
-  "storageBucket": "studio-5177963256-f3dc5.appspot.com",
+  "authDomain": "vesotel-gestor-jornada.firebaseapp.com",
+  "storageBucket": "vesotel-gestor-jornada.appspot.com",
   "messagingSenderId": "1088612949766"
 };
