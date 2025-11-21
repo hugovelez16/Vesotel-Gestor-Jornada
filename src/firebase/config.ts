8f1959dcc326776cab584247b3187ab8b434a0c2
Hugo Vélez <hugo@vesotel.com>
2025-11-21 10:31:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5177963256-f3dc5",
+  "appId": "1:999945211726:web:6c57e830bdf5bef4e3d81f",
+  "apiKey": "AIzaSyD2Vbp9l_CdtPyzKkgpLKA_aay381Qdth4",
+  "authDomain": "studio-5177963256-f3dc5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "999945211726"
+};
